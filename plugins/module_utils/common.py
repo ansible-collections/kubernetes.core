a38cbc823746e76efe0c666440b09e8e3d9b15aa
Alina Buzachis <abuzachis@redhat.com>
2021-04-08 16:28:47 +0200

    * Replicate apply method in the DynamicClient
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/module_utils/common.py
+++ plugins/module_utils/common.py
@@ -43,7 +43,6 @@ K8S_IMP_ERR = None
 try:
     import kubernetes
     import openshift
-    from openshift.dynamic import DynamicClient
     from openshift.dynamic.exceptions import (
         ResourceNotFoundError, ResourceNotUniqueError, NotFoundError, DynamicApiError,
         ConflictError, ForbiddenError, MethodNotAllowedError)
@@ -101,6 +100,21 @@ except ImportError as e:
     HAS_K8S_INSTANCE_HELPER = False
     k8s_import_exception = e
     K8S_IMP_ERR = traceback.format_exc()
+
+class DynamicClient(kubernetes.dynamic.DynamicClient):
+    def apply(self, resource, body=None, name=None, namespace=None):
+        body = super().serialize_body(body)
+        body['metadata'] = body.get('metadata', dict())
+        name = name or body['metadata'].get('name')
+        if not name:
+            raise ValueError("name is required to apply {}.{}".format(resource.group_version, resource.kind))
+        if resource.namespaced:
+            body['metadata']['namespace'] = super().ensure_namespace(resource, namespace, body)
+        try:
+            return apply(resource, body)
+        except ApplyException as e:
+            raise ValueError("Could not apply strategic merge to %s/%s: %s" %
+                             (body['kind'], body['metadata']['name'], e))
 
 
 def configuration_digest(configuration):
