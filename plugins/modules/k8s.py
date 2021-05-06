63605006ade0c927ace61f484d7f43f7f4d40a82
Mike Graves <mgraves@redhat.com>
2021-05-06 08:38:10 -0400

    Update plugins/modules/k8s.py
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/modules/k8s.py
+++ plugins/modules/k8s.py
@@ -45,7 +45,7 @@ options:
     - For example, Custom Resource Definitions typically aren't updatable by the usual strategic merge. You may
       want to use C(merge) if you see "strategic merge patch format is not supported"
     - See U(https://kubernetes.io/docs/tasks/run-application/update-api-object-kubectl-patch/#use-a-json-merge-patch-to-update-a-deployment)
-    - If more than one merge_type is given, the merge_types will be tried in order. This defaults to
+    - If more than one C(merge_type) is given, the merge_types will be tried in order. This defaults to
       C(['strategic-merge', 'merge']), which is ideal for using the same parameters on resource kinds that
       combine Custom Resources and built-in resources.
     - mutually exclusive with C(apply)
