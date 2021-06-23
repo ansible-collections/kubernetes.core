51b9173c868c14451537b63e61e0dcd864c1ce3e
Mike Graves <mgraves@redhat.com>
2021-06-23 17:37:34 -0400

    Fix linting issue

--- plugins/module_utils/ansiblemodule.py
+++ plugins/module_utils/ansiblemodule.py
@@ -3,4 +3,4 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import AnsibleModule  # noqa: F401
