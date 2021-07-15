28083c3222e5e5633317e709fe780046eb1572e3
Mike Graves <mgraves@redhat.com>
2021-07-15 10:33:48 -0400

    Re-enable support for turbo mode
    This re-enables the ability to add turbo mode. It also adds a few more
    tests to cover some cases that had been broken in turbo mode previously.

--- plugins/module_utils/ansiblemodule.py
+++ plugins/module_utils/ansiblemodule.py
@@ -3,4 +3,10 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-from ansible.module_utils.basic import AnsibleModule  # noqa: F401
+try:
+    from ansible_collections.cloud.common.plugins.module_utils.turbo.module import (
+        AnsibleTurboModule as AnsibleModule,
+    )  # noqa: F401
+    AnsibleModule.collection_name = "kubernetes.core"
+except ImportError:
+    from ansible.module_utils.basic import AnsibleModule  # noqa: F401
