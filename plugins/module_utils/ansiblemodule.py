7f85d58604947981da620c092624ab2ee96d4117
Mike Graves <mgraves@redhat.com>
2021-08-02 10:31:44 -0400

    Use environment variable to enable turbo mode

--- plugins/module_utils/ansiblemodule.py
+++ plugins/module_utils/ansiblemodule.py
@@ -3,10 +3,22 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
+import os
+
+from ansible.module_utils.common.validation import check_type_bool
+
 try:
-    from ansible_collections.cloud.common.plugins.module_utils.turbo.module import (
-        AnsibleTurboModule as AnsibleModule,
-    )  # noqa: F401
-    AnsibleModule.collection_name = "kubernetes.core"
-except ImportError:
+    enable_turbo_mode = check_type_bool(os.environ.get("ENABLE_TURBO_MODE"))
+except TypeError:
+    enable_turbo_mode = False
+
+if enable_turbo_mode:
+    try:
+        from ansible_collections.cloud.common.plugins.module_utils.turbo.module import (
+            AnsibleTurboModule as AnsibleModule,
+        )  # noqa: F401
+        AnsibleModule.collection_name = "kubernetes.core"
+    except ImportError:
+        from ansible.module_utils.basic import AnsibleModule  # noqa: F401
+else:
     from ansible.module_utils.basic import AnsibleModule  # noqa: F401
