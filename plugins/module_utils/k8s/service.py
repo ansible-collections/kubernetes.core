2c08ae4af4404f350dd0e7e4ee876ac9f61490e0
Alina Buzachis <alinabuzachis@gmail.com>
2022-01-12 12:37:49 +0100

    Remove unused imports

--- plugins/module_utils/k8s/service.py
+++ plugins/module_utils/k8s/service.py
@@ -1,8 +1,6 @@
 # Copyright: (c) 2021, Red Hat | Ansible
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-import time
-from datetime import datetime
 from typing import Any, Dict, List, Optional, Tuple
 
 from ansible_collections.kubernetes.core.plugins.module_utils.hashes import (
