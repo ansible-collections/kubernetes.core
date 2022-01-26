28b87699a9e9979389b15cef2fc2c14933434cf8
Alina Buzachis <alinabuzachis@gmail.com>
2022-01-27 00:00:16 +0100

    Update client.py

--- plugins/module_utils/k8s/client.py
+++ plugins/module_utils/k8s/client.py
@@ -1,7 +1,6 @@
 # Copyright: (c) 2021, Red Hat | Ansible
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from http import client
 import os
 import hashlib
 from typing import Any, Dict, List, Optional
