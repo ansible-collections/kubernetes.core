30f610776875fed33814c507ea1e0e25b5426d90
Christian von Stebut <christian@von.stebut.org>
2022-01-27 17:51:16 +0100

    fix linting issues, take 2

--- tests/unit/modules/test_helm_template.py
+++ tests/unit/modules/test_helm_template.py
@@ -6,13 +6,9 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-import os.path
-
 import argparse
 
-from ansible_collections.kubernetes.core.plugins.modules.helm_template import (
-    template,
-)
+from ansible_collections.kubernetes.core.plugins.modules.helm_template import template
 
 
 def test_template_with_release_values_and_values_files():
@@ -20,25 +16,20 @@ def test_template_with_release_values_and_values_files():
     helm_cmd = "helm"
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('cmd')
-    parser.add_argument('template')
-# to "simulate" helm template options, include two optional parameters NAME and CHART.
-# if parsed string contains only one parameter, the value will be passed
-# to CHART and NAME will be set to default value "release-name" as in helm template
-    parser.add_argument('NAME', nargs='?', default="release-name")
-    parser.add_argument('CHART', nargs='+')
-    parser.add_argument('-f', action='append')
+    parser.add_argument("cmd")
+    parser.add_argument("template")
+    # to "simulate" helm template options, include two optional parameters NAME and CHART.
+    # if parsed string contains only one parameter, the value will be passed
+    # to CHART and NAME will be set to default value "release-name" as in helm template
+    parser.add_argument("NAME", nargs="?", default="release-name")
+    parser.add_argument("CHART", nargs="+")
+    parser.add_argument("-f", action="append")
 
-    rv = {
-        "v1": {
-            "enabled": True
-        }
-    }
-    vf = [
-        "values1.yml",
-        "values2.yml"
-    ]
-    mytemplate = template(cmd=helm_cmd, chart_ref=my_chart_ref, release_values=rv, values_files=vf)
+    rv = {"v1": {"enabled": True}}
+    vf = ["values1.yml", "values2.yml"]
+    mytemplate = template(
+        cmd=helm_cmd, chart_ref=my_chart_ref, release_values=rv, values_files=vf
+    )
 
     args, unknown = parser.parse_known_args(mytemplate.split())
 
