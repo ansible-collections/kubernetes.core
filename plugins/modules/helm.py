e0af7b093cace768e82685aeb8c897aa9c6b837c
Abhijeet Kasurde <akasurde@redhat.com>
2022-02-15 14:19:25 +0530

    helm: list all releases
    While performing uninstall, list all releases to gather information
    about them.
    
    Fixes: #319
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/modules/helm.py
+++ plugins/modules/helm.py
@@ -354,7 +354,7 @@ def get_release_status(module, command, release_name):
     Get Release state from deployed release
     """
 
-    list_command = command + " list --output=yaml --filter " + release_name
+    list_command = command + " list --all --output=yaml --filter " + release_name
 
     rc, out, err = run_helm(module, list_command)
 
