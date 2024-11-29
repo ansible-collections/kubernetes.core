82f00df724978d540d83782038cb10fa0143db5d
Yuriy Novostavskiy <yuriy@novostavskiy.kiev.ua>
2024-11-29 14:17:30 +0000

    reformat plugins/module_utils/helm.py with black
    to fix linters in actions

--- plugins/module_utils/helm.py
+++ plugins/module_utils/helm.py
@@ -163,7 +163,9 @@ class AnsibleHelmModule(object):
         if not HAS_YAML:
             self.fail_json(msg=missing_required_lib("PyYAML"), exception=YAML_IMP_ERR)
 
-        rc, out, err = self.run_command(command, environ_update=self.env_update, data=data)
+        rc, out, err = self.run_command(
+            command, environ_update=self.env_update, data=data
+        )
         if fails_on_error and rc != 0:
             self.fail_json(
                 msg="Failure when executing Helm command. Exited {0}.\nstdout: {1}\nstderr: {2}".format(
