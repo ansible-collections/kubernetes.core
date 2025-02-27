324bc52962abdcf980c6c97af980a7a3c9dd62cd
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2025-02-27 13:25:55 +0100

    Update tests/unit/module_utils/test_hide_fields.py

--- tests/unit/module_utils/test_hide_fields.py
+++ tests/unit/module_utils/test_hide_fields.py
@@ -179,9 +179,6 @@ def test_hiding_nested_dicts_using_brackets():
             ]
         ),
     )
-    if hide_fields(output, hidden_fields) != expected:
-        print(output)
-        print(expected)
     assert hide_fields(output, hidden_fields) == expected
 
 
