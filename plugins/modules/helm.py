a700e8478d20808a43411fc25ad1a0290e2bdd33
Bikouo Aubin <79859644+abikouo@users.noreply.github.com>
2026-01-14 09:22:31 +0100

    address sanity issues (#1056)
    SUMMARY
    
    helm/helm_info - Deprecate some parameters and add new ones to resolve sanity issues.
    k8s - the return block doc is not aligned with what the module returns
    
    ISSUE TYPE
    
    Bugfix Pull Request
    
    COMPONENT NAME
    
    helm, helm_info, k8s
    Fixes: #1046
    
    Reviewed-by: Yuriy Novostavskiy <yuriy@novostavskiy.kyiv.ua>
    Reviewed-by: Bianca Henderson <beeankha@gmail.com>
    (cherry picked from commit bd1cacc0cf2be23f170b015e5adad0435ee7bfb8)

--- plugins/modules/helm.py
+++ plugins/modules/helm.py
@@ -392,9 +392,18 @@ status:
       returned: always
       description: The Date of last update
     values:
-      type: str
+      type: dict
       returned: always
-      description: Dict of Values used to deploy
+      description:
+        - Dict of Values used to deploy.
+        - This return value has been deprecated and will be removed in a release after
+          2027-01-08. Use RV(status.release_values) instead.
+    release_values:
+      type: dict
+      returned: always
+      description:
+        - Dict of Values used to deploy.
+      version_added: 6.3.0
 stdout:
   type: str
   description: Full `helm` command stdout, in case you want to display it or examine the event log
@@ -474,7 +483,8 @@ def get_release_status(module, release_name, all_status=False):
     if release is None:  # not install
         return None
 
-    release["values"] = module.get_values(release_name)
+    release["release_values"] = module.get_values(release_name)
+    release["values"] = release["release_values"]
 
     return release
 
@@ -1030,10 +1040,15 @@ def main():
                 changed = True
 
     if module.check_mode:
-        check_status = {"values": {"current": {}, "declared": {}}}
+        check_status = {
+            "values": {"current": {}, "declared": {}},
+            "release_values": {"current": {}, "declared": {}},
+        }
         if release_status:
-            check_status["values"]["current"] = release_status["values"]
+            check_status["values"]["current"] = release_status["release_values"]
             check_status["values"]["declared"] = release_status
+            check_status["release_values"]["current"] = release_status["release_values"]
+            check_status["release_values"]["declared"] = release_status
 
         module.exit_json(
             changed=changed,
